07e8946ff128ec9e2e41d86f6c340dfe9c28a540
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2025-04-11 14:17:12 +0530

    Update Team.jsx

--- Frontend/src/pages/Team.jsx
+++ Frontend/src/pages/Team.jsx
@@ -39,19 +39,19 @@ const Team = () => {
 
   return (
     <div className="relative w-full min-h-screen bg-gradient-to-tr from-black via-zinc-900 to-slate-900 overflow-hidden text-white">
-      {/* <div className="absolute top-[7%] left-1/2 translate-x-[-50%] translate-y-[-50%] z-50">
+      <div className="absolute top-[7%] left-1/2 translate-x-[-50%] translate-y-[-50%] z-50">
         <Navbar />
-      </div> */}
+      </div>
 
       {/* Background text */}
-      {/* <h1 className="absolute hidden lg:inline lg:text-[14vw] font-extrabold text-white/5 lg:top-[15%] left-[35%] lg:left-1/4 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none z-0">
+      <h1 className="absolute hidden lg:inline lg:text-[14vw] font-extrabold text-white/5 lg:top-[15%] left-[35%] lg:left-1/4 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none z-0">
         THE
       </h1>
       <h1 className="absolute hidden lg:inline lg:text-[18vw] font-extrabold text-white/5 lg:top-[40%] left-[62%] lg:left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none z-0">
         TEAM
-      </h1> */}
+      </h1>
 
-      {/* Desktop layout
+      {/* Desktop layout*/}
       <div className="h-full px-20 mt-20 hidden lg:block">
         {teamMembers.map((member, index) => (
           <div
@@ -94,10 +94,10 @@ const Team = () => {
             </div>
           </div>
         ))}
-      </div> */}
+      </div> 
 
       {/* Mobile layout with smaller cards */}
-      {/* <div className="lg:hidden sm:items-center flex flex-col pl-8 gap-10 mt-32">
+      <div className="lg:hidden sm:items-center flex flex-col pl-8 gap-10 mt-32">
         {teamMembers.map((member, index) => (
           <div key={index} className="w-[200px] flex text-center">
             <TiltedCard
@@ -136,7 +136,7 @@ const Team = () => {
             </div>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   )
 }
